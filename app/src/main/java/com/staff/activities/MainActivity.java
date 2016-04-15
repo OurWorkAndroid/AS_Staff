dbf455b3da3b6954933958851a31438389a620d2
dengyaoning420 <DENGYAONING420@pingan.com.cn>
2016-04-15 18:12:36 +0800

    增加BadgeView功能(控件右上角能显示数字)

--- app/src/main/java/com/staff/activities/MainActivity.java
+++ app/src/main/java/com/staff/activities/MainActivity.java
@@ -2,15 +2,28 @@ package com.staff.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.jauker.widget.BadgeView;
 import com.staff.main.R;
 
 public class MainActivity extends Activity {
-
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_test = (Button)findViewById(R.id.btn_test);
+        final BadgeView badgeView = new BadgeView(this);
+        badgeView.setTargetView(btn_test);
+        badgeView.setBadgeCount(++count);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                badgeView.setBadgeCount(++count);
+            }
+        });
     }
 
 }
